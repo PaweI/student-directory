4a68ed6c74731c6a9a11bbf1f90c3fdee6bf1eba
Pavel Redics <pavel.redics@gmail.com>
2014-09-28 21:47:16 +0100

    Put students into array, add interpolation in last line

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 student_count = 15
+#put students into array
+students = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 #first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 "------------------"
-puts 1
-puts 2
-puts 3
-puts 4
-puts 5
-puts 6
-puts 7
-puts 8
-puts 9
-puts 10
-puts 11
-puts 12
-puts 13
-puts 14
-puts 15
+#now print them 
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
 #then we print total
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print student_count
-puts " prospective students"
+puts "Overall, we have #{students.length} prospective students"
