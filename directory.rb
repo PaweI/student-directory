7800d36f765289500d14721105e1108f2b44342a
Pavel Redics <pavel.redics@gmail.com>
2014-09-28 21:36:39 +0100

    Add comments, change empty string to integer, change count to variable count

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
+student_count = 15
+#first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 "------------------"
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
-puts ""
+puts 1
+puts 2
+puts 3
+puts 4
+puts 5
+puts 6
+puts 7
+puts 8
+puts 9
+puts 10
+puts 11
+puts 12
+puts 13
+puts 14
+puts 15
+#then we print total
 print "Overall, we have "
-print 15
+#it's important that print() doesn't add new line characters
+print student_count
 puts " prospective students"
