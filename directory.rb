b3699c6b51ffa7071d4e7d726efe10d0df36295f
Pavel Redics <pavel.redics@gmail.com>
2014-09-28 22:04:43 +0100

    Added iteration thru array

--- directory.rb
+++ directory.rb
@@ -1,24 +1,11 @@
-student_count = 15
 #put students into array
 students = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 #first, we print the list of students
 puts "The students of my cohort at Makers Academy"
 "------------------"
 #now print them 
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
+students.each do |student|
+	puts student
+end
 #then we print total
 puts "Overall, we have #{students.length} prospective students"
