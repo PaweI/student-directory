e466cbd5a3a0dd440deb05ec94692179f0dd5be4
Pavel Redics <pavel.redics@gmail.com>
2014-09-29 20:57:16 +0100

    Swap list of array to dict, add cohort

--- directory.rb
+++ directory.rb
@@ -1,19 +1,35 @@
 #put students into array
-students = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+students = [
+	{:name => 1, :cohort => :october},
+	{:name => 2, :cohort => :october},
+	{:name => 3, :cohort => :october},
+	{:name => 4, :cohort => :october},
+	{:name => 5, :cohort => :october},
+	{:name => 6, :cohort => :october},
+	{:name => 7, :cohort => :october},
+	{:name => 8, :cohort => :october},
+	{:name => 9, :cohort => :october},
+	{:name => 10, :cohort => :october},
+	{:name => 11, :cohort => :october},
+	{:name => 12, :cohort => :october},
+	{:name => 13, :cohort => :october},
+	{:name => 14, :cohort => :october},
+	{:name => 15, :cohort => :october}
+]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
-	"------------------"
+	puts "------------------"
 end
 
-def print(names) 
-	names.each do |name|
-		puts name
+def print(students) 
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} prospective students"
+	puts "Overall, we have #{names.length} great students"
 end
 
 #lets call this methods
