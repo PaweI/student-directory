56145773eec6c0380af1771904808dbf3c14c7f9
Pavel Redics <pavel.redics@gmail.com>
2014-10-05 00:03:45 +0100

    Change footer to show students by cohors, make variable students global

--- directory.rb
+++ directory.rb
@@ -6,22 +6,31 @@ def input_cohort
     cohort = gets.chomp.to_sym
     months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
 	if cohort == :""
-	    cohort = :october
+		cohort = :october
+		return cohort
 	elsif months.include?(cohort)
-	    nil
+	    return cohort
 	else
 		puts "Sorry, there maybe some mistake in your input."
 		# new input using recursive method
 		input_cohort
 	end
-	cohort
 end
+
+def plural_form(students)
+	if @students.length == 1
+		return "student"
+	else 
+		return "students"
+	end
+end
+
 
 def input_students
 	puts "Please enter the names of the students".center(52, '-') 
 	puts "To finish, just hit return twice".center(46, '-')
 	# let's create an empty array
-	students = []
+	@students = []
 	# get info
 	puts "Enter your name:"
 	name = gets.chomp
@@ -35,20 +44,15 @@ def input_students
         hobbies = gets.chomp
 		# add student hash to array
 		# default value of cohort
-		students << {:name => name, :cohort => cohort, :age => age, :country_of_birth => country_of_birth,
+		@students << {:name => name, :cohort => cohort, :age => age, :country_of_birth => country_of_birth,
 		:hobbies => hobbies}
-		if students.length == 1
-			plural_form = "student"
-		else 
-			plural_form = "students"
-		end
-		puts "Now we have #{students.length} #{plural_form}"
+		puts "Now we have #{@students.length} #{plural_form(@students)}"
 		# get another name from the user
 		puts "Enter your name:"
 		name = gets.chomp
 	end
 	# return array of students
-	students
+	@students
 end
 
 def print_header
@@ -57,13 +61,25 @@ def print_header
 end
 
 def print(students) 
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	#students.each do |student|
+		#puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	#end
+	cohorts = []
+	@students.each do |student|
+		cohorts << student[:cohort] if cohorts.include?(student[:cohort]) != true
+	end
+	cohorts.each do |cohort|
+		puts "#{cohort} cohort:"
+		@students.each do |student|
+			if cohort == student[:cohort]
+				puts "#{student[:name]}"
+			end
+		end
 	end
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	puts "Overall, we have #{names.length} great #{plural_form(@students)}"
 end
 
 # print name start with input letter
